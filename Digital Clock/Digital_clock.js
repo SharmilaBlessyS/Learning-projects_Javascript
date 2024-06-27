3ce4439b5aad4cf93363613fbe5d46065767775e
SharmilaBlessyS <sharmila@kalakendradao.org>
2024-06-27 11:40:48 +0530

    projects

--- Digital Clock/Digital_clock.js
+++ Digital Clock/Digital_clock.js
@@ -5,8 +5,7 @@ let seconds = document.getElementById('sec');
 
 setInterval(()=>{
     let time = new Date();
-    console.log(time);
     hours.innerHTML = (time.getHours()<10 ? "0": "")+ time.getHours();
     minutes.innerHTML = (time.getMinutes()<10 ? "0": "")+time.getMinutes();
     seconds.innerHTML = (time.getSeconds()<10 ? "0": "")+time.getSeconds();
-},1000);
+},10);
--- Email_subscription/email.js
+++ Email_subscription/email.js
@@ -1,16 +1,24 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbznf6BGUDxqWZq0ehvE7quJjVDAmKgFQlu5x96NxRv0GO0cYBPZVIv66TGpmqtSbLLH/exec'
 const form = document.forms['submit-to-google-sheet']
 const msg = document.getElementById('msg');
+const emailInput = document.getElementById('text');
+const display = document.getElementById('display');
+
 
 form.addEventListener('submit', e => {
-  e.preventDefault()
+  e.preventDefault();
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response => {
         msg.innerHTML = "Thank You for Subscribing!";
         setTimeout(function(){
             msg.innerHTML = "";
-        },5000)
-        form.reset()
+        }, 5000)
+        form.reset();
+        display.textContent = ''; // Clear display after form submission
     })
     .catch(error => console.error('Error!', error.message))
-})
+});
+
+emailInput.addEventListener('input', function(e) {
+  display.textContent = e.target.value;
+});
